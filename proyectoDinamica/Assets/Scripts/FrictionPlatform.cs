a6803806acb1fe47ce825471944c5128f1a7addc
Roberto Frías Bernal <roberfrias115@gmail.com>
2018-05-25 14:28:29 +0200

    Fast jump, jump like mario 64 done, friction platforms gives exception

---
+++ proyectoDinamica/Assets/Scripts/FrictionPlatform.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrictionPlatform : MonoBehaviour {
+
+    [Range(0.01f, 1.0f)]
+    public float friction;
+
+    private float playerBaseAcceleration = -1, playerBaseDeceleration = -1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == Layers.Player)
+        {
+            if(playerBaseAcceleration == -1)
+            {
+                playerBaseAcceleration = other.GetComponent<PlayerMovementController>().GetbaseAcceleration();
+            }
+
+            if (playerBaseDeceleration == -1)
+            {
+                playerBaseDeceleration = other.GetComponent<PlayerMovementController>().GetbaseDeceleration();
+            }
+
+            other.GetComponent<PlayerMovementController>().Acceleration = playerBaseAcceleration * friction;
+            other.GetComponent<PlayerMovementController>().Deceleration = playerBaseDeceleration * friction;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer == Layers.Player)
+        {
+            other.GetComponent<PlayerMovementController>().Acceleration = playerBaseAcceleration * friction;
+            other.GetComponent<PlayerMovementController>().Deceleration = playerBaseDeceleration * friction;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == Layers.Player)
+        {
+            other.GetComponent<PlayerMovementController>().Acceleration = playerBaseAcceleration;
+            other.GetComponent<PlayerMovementController>().Deceleration = playerBaseDeceleration;
+        }
+    }
+}
--- proyectoDinamica/Assets/Scripts/PlayerMovementController.cs
+++ proyectoDinamica/Assets/Scripts/PlayerMovementController.cs
@@ -4,20 +4,65 @@ using UnityEngine;
 
 public class PlayerMovementController : MovementController {
 	public GameObject BombPrefab;
+    public float fastJumpTimeThreshold= 1;
 
-	protected override Vector2 GetDesiredMovement () {
+    private float fastJumpTimer;
+    private int numberOfJumps = 0;
+    private float baseAcceleration, baseDeceleration;
+
+
+    public void Start()
+    {
+        fastJumpTimer = fastJumpTimeThreshold;
+        baseAcceleration = Acceleration;
+        baseDeceleration = Deceleration;
+    }
+
+    protected override Vector2 GetDesiredMovement () {
 		return new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical")).normalized;
 	}
 
 	protected override void Update() {
 		base.Update ();
 		if (Input.GetKeyDown (KeyCode.Space) && _groundCheck.Grounded) {
-			// Salto
+            // Salto
+            //Debug.Log("Salto normal");
+            numberOfJumps = 1;
 			_rb.AddForce(Vector3.up * JumpForce, ForceMode.VelocityChange);
-		}
+		} else if (Input.GetKeyDown(KeyCode.Space) && !_groundCheck.Grounded)
+        {
+            fastJumpTimer = 0;
+        } else if (_groundCheck.Grounded && fastJumpTimer < fastJumpTimeThreshold)
+        {
+            //Debug.Log("Salto rapido");
+            _rb.AddForce(Vector3.up * (JumpForce + (numberOfJumps * 3)) , ForceMode.VelocityChange);
+            //Debug.Log("Numero de salto: " + numberOfJumps);
+            if (numberOfJumps == 3)
+            {
+                numberOfJumps = 1;
+            }
+            else
+            {
+                numberOfJumps++;
+
+            }
+            fastJumpTimer = fastJumpTimeThreshold;
+        }
+
+        fastJumpTimer += Time.deltaTime;
 
 		if (Input.GetKeyDown (KeyCode.Q)) {
 			Instantiate (BombPrefab, transform.position, Quaternion.identity);
 		}
 	}
+
+    public float GetbaseAcceleration()
+    {
+        return baseAcceleration;
+    }
+
+    public float GetbaseDeceleration()
+    {
+        return baseDeceleration;
+    }
 }
